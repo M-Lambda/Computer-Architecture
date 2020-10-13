a4af57d9760a486e76ed1a6816aafcd14a3791e0
MaryamMosstoufi <maryam@zooxoos.com>
2020-10-13 12:59:09 -0400

    Step 1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,12 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        # 8 general-purpose registers
+        self.reg = [0] * 8
+        # 256 bytes of memory
+        self.ram = [0] * 256
+        # program counter, to keep track of the address of currently executing instruction
+        self.pc = 0
 
     def load(self):
         """Load a program into memory."""
@@ -30,13 +35,12 @@ class CPU:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +52,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
